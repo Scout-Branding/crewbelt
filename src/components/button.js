0f49871692308b3d12d4c8a0b4b47e15523154c1
Joshua Fredrickson <joshua@scoutbrand.com>
2021-10-29 12:53:59 -0500

    Add Header, Button

---
+++ src/components/button.js
@@ -0,0 +1,35 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import tw, { styled } from "twin.macro"
+
+const ButtonEl = styled.a`
+  ${tw`inline-block px-6 py-2 text-sm tracking-wide text-white uppercase transition transform translate-y-0 rounded-full cursor-pointer bg-brand-secondary`}
+
+  &:hover {
+    ${tw`shadow-lg`}
+    --tw-translate-y: -3px;
+  }
+
+  ${props => props.type === "primary" && tw`bg-brand-primary`}
+  ${props => props.type === "secondary" && tw`bg-brand-secondary`}
+`
+
+const Button = ({ className, children, type }) => {
+  return (
+    <ButtonEl className={className} {...{ type }}>
+      {children}
+    </ButtonEl>
+  )
+}
+
+Button.defaultProps = {
+  type: "primary",
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.string,
+  className: PropTypes.string,
+}
+
+export default Button
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,72 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import tw, { styled } from "twin.macro"
 import { Link } from "gatsby"
+import Container from "./container"
+import logo from "../images/crewbelt-logo.svg"
+import iconIos from "../images/icon-ios.svg"
+import iconWindows from "../images/icon-windows.svg"
+import Button from "./button"
+
+const Brand = styled(Link)`
+  ${tw`block w-44`}
+`
+
+const NavContainer = styled(Container)`
+  ${tw`flex items-end justify-between py-24`}
+`
+
+const LinkUnderline = styled.a`
+  ${tw`inline-block font-extrabold leading-loose tracking-wider uppercase transition-colors text-brand-primary`}
+
+  &::after {
+    ${tw`block w-full h-1 transition-colors rounded-sm bg-brand-primary`}
+    content: '';
+  }
+
+  &:hover {
+    ${tw`text-brand-secondary`}
+
+    &::after {
+      ${tw`bg-brand-secondary`}
+    }
+  }
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <nav aria-label="Primary">
+      <NavContainer>
+        <Brand to="/">
+          <img src={logo} alt={siteTitle} />
+        </Brand>
+        <div>
+          <LinkUnderline href="#1" target="_blank" rel="nofollow noopener">
+            Project Manager Portal
+          </LinkUnderline>
+        </div>
+      </NavContainer>
+    </nav>
+    <Container>
+      <div tw="w-1/2">
+        <h1 tw="font-display uppercase text-6xl 2xl:text-7xl mb-6">
+          Go from Connection to Construction, on Demand.
+        </h1>
+        <p tw="text-xl mb-6">
+          CrewBelt is the only digital employment platform that instantly
+          connects independent electricians with contractors who need skilled
+          talent on jobsites.
+        </p>
+        <Button type="secondary" tw="mr-2 inline-flex items-center flex-row">
+          <img src={iconIos} alt="" tw="w-4 h-4 mr-2" />
+          Download for iOS
+        </Button>
+        <Button tw="inline-flex items-center flex-row">
+          <img src={iconWindows} alt="" tw="w-4 h-4 mr-2" />
+          Download for Windows
+        </Button>
+      </div>
+    </Container>
   </header>
 )
 

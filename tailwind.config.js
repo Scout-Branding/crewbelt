058d6fb076a82d8520a68cca5f25b523bf259b99
Joshua Fredrickson <joshua@scoutbrand.com>
2021-10-29 12:51:33 -0500

    Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
 const { fontFamily } = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     fontFamily: {
       sans: ["Mulish", ...fontFamily.sans],
       display: ["neusa-next-std-compact", ...fontFamily.sans],
     },
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#4164ff",
+          primaryFaded: "#afc8ff",
+          secondary: "#32328c",
+          black: "#323232",
+          highlight: {
+            contractor: "#ffb90a",
+            manager: "#e62d37",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }

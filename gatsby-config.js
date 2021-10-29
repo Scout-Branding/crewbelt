7d16113f20b6bf3d6cbc387239f09414c2982540
Joshua Fredrickson <joshua@scoutbrand.com>
2021-10-29 10:43:14 -0500

    Init favicons and site manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `CrewBelt`,
+    description: `Go from connection to construction, on demand.`,
+    author: `@joshuafredrickson`,
+    siteUrl: `https://crewbelt.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,15 +20,26 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `CrewBelt`,
+        short_name: `CrewBelt`,
+        description: `Go from connection to construction, on demand.`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#e6e6e6`,
+        theme_color: `#e6e6e6`,
+        display: `standalone`,
+        icon: `src/images/favicons/apple-touch-icon.png`,
+        icons: [
+          {
+            src: `src/images/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicons//android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
     `gatsby-plugin-emotion`,

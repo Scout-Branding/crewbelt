fe381e63fc9bbc949f63afce3274da88b63dc586
Joshua Fredrickson <joshua@scoutbrand.com>
2021-12-02 16:20:51 -0600

    Change portal link URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Go from connection to construction, on demand.`,
     author: `@scoutbranding`,
     siteUrl: `https://crewbelt.com/`,
-    portalLink: `https://www.crewbelt.com/account/sign-in`,
+    portalLink: `https://app.crewbelt.com/account/sign-in`,
     appLinks: {
       microsoft: `https://www.microsoft.com/en-us/p/dropbox-for-s-mode/9wzdncrfj0pk`,
       ios: `https://apps.apple.com/us/app/crewbelt/id1580751891`,
